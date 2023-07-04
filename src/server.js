046017d0176d0042fba5a2006f3f5d4f43225ca7
suhailtajshaik <suhailtajshaik@gmail.com>
2023-07-03 21:49:23 -0500

    added try catch block

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 // require("dotenv").config();
-const config = require("../config");
+const config = require("../config/index");
 const watch = require("node-watch");
 const fetch = require("node-fetch");
 const nodePath = require("path");
@@ -12,6 +12,7 @@ const upload = multer({ dest: "uploads/" });
 const app = express();
 
 const SYNC_FROM_DIR = config.SYNC_FROM_DIR;
+console.log("Syncing files from ", SYNC_FROM_DIR);
 const SYNC_TO_DIR = config.SYNC_TO_DIR;
 const createFolder = (folderName) => {
   // Check if folder exists else create folder
@@ -24,17 +25,18 @@ createFolder(SYNC_FROM_DIR);
 createFolder(SYNC_TO_DIR);
 
 watch(SYNC_FROM_DIR, { recursive: false }, async (evt, name) => {
-  const file = fs.createReadStream(name);
-
-  const form = new FormData();
-  form.append("file", file);
-
-  await fetch(`${process.env.REMOTE_URL}/files`, {
-    method: "POST",
-    body: form
-  });
-
-  fs.unlinkSync(name);
+  try {
+    const file = fs.createReadStream(name);
+    const form = new FormData();
+    form.append("file", file);
+    await fetch(`${process.env.REMOTE_URL}/files`, {
+      method: "POST",
+      body: form
+    });
+    fs.unlinkSync(name);
+  } catch (error) {
+    console.log("error", error);
+  }
 });
 
 app.post("/files", upload.single("file"), function (req, res, next) {
